c01cfc2ced40eef6a237dc53521664a9de0c8864
TriBhaskar <panthribhaskar@gmail.com>
2023-11-29 21:00:55 +0530

    changing heading with using useState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleNameChange(event) {
+    setSubmitted(false); //bcoz name is change after every key strok
+    setPlayerName(event.target.value);
+  }
+  function handleClick() {
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {submitted ? (
+        <h2>Welcome {playerName}</h2>
+      ) : (
+        <h2>Welcome unknown entity</h2>
+      )}
+
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
