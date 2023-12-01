62046eb232da402b19e0e02c43962cb127e65b18
TriBhaskar <panthribhaskar@gmail.com>
2023-12-01 22:36:36 +0530

    Done most of the things

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -6,8 +6,9 @@ const ResultModal = forwardRef(function ResultModal(
 ) {
   const dialog = useRef();
   const userLost = remainingTime <= 0;
-
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = (1 - remainingTime / (targetTime * 1000)) * 100; //prettier-ignore
+
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -16,8 +17,9 @@ const ResultModal = forwardRef(function ResultModal(
     };
   });
   return (
-    <dialog ref={dialog} className="result-modal">
-      {userLost && <h2>You </h2>}
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your Score : {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
       </p>
